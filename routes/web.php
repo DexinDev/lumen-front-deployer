ab52254e67bc61630c6efe4080fb07699028d6f9
Golovizin Danil <danil@rutorika.ru>
2020-09-14 16:12:38 +0300

    try hooks

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@
 $router->get('/', ['uses' => 'ReposListController@viewLists', "as" => "index"]);
 $router->get('/deploy/{branch}', ['uses' => 'DeployController@deploy', "as" => "deploy"]);
 $router->get('/delete/{branch}', ['uses' => 'BranchDeleteController@delete', "as" => "delete"]);
+$router->post('/hook/update', ['uses' => 'HookController@updateHook', "as" => "hookupdate"]);
 $router->get('/hook/update', ['uses' => 'HookController@updateHook', "as" => "hookupdate"]);
