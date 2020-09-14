ab6691a53cf9a21e57528ee9553a2138b9688c77
Golovizin Danil <danil@rutorika.ru>
2020-09-14 15:58:47 +0300

    add log info

--- clone-rep.sh
+++ clone-rep.sh
@@ -3,12 +3,12 @@ BASEDIR=$(dirname $0)
 DIRREPNAME=${1,,}
 cd "${BASEDIR}/../reps/"
 cp -r master ${DIRREPNAME}
-echo "copy done"
+echo "copy done ${DIRREPNAME}"
 cd $1
 git checkout $1
-echo "checkout done"
+echo "checkout done ${1}"
 git pull origin $1
-echo "pull done"
+echo "pull done ${1}"
 cd markup
 npm i
 echo "npm i done"
