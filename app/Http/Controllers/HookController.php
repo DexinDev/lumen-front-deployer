4b6b6a2b29ad5ef5bf300db733baa2b55a042bf2
Golovizin Danil <danil@rutorika.ru>
2020-09-14 16:26:32 +0300

    try hooks

--- app/Http/Controllers/HookController.php
+++ app/Http/Controllers/HookController.php
@@ -8,7 +8,7 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class HookController extends BaseController
 {
     public function updateHook(){
-        Log::debug("EVENT NAME" . request()->event_name );
+        Log::debug("EVENT NAME" . basename(request()->ref) );
 //        Log::debug("catch request", request()->json());
     }
 }
